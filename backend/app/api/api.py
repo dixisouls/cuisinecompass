e3e044b8c8732ab7f3064b434c6ce96c97c1b7fe
Divya <divyadli201@gmail.com>
2025-04-09 16:19:13 -0700

    Removed backend

--- backend/app/api/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.api.endpoints import auth, users, recipes, dashboard
-
-api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(recipes.router, prefix="/recipes", tags=["recipes"])
-api_router.include_router(dashboard.router, prefix="/dashboard", tags=["dashboard"])
